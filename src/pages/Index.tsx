badfa0e096d8695129f6c879bf29d7454476097e
Lenin Aviles <lenin.aviles@hearst.com>
2025-05-26 06:22:59 -0700

    fix: update hero carousel layout and component order
    - Adjust hero carousel aspect ratio to 4:6 on mobile, 16:9 on desktop
    - Remove vertical padding from hero carousel
    - Reorder components to place VehicleFinder after BuyersGuide
    - Update spacing between components to 16px

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -12,41 +12,41 @@ const Index = () => {
   return <div className="flex min-h-screen flex-col bg-motortrend-gray w-full">
       <GlobalHeader />
       <main className="flex flex-1 flex-col">
-        <div className="max-w-[980px] mx-auto px-4 sm:px-6 lg:px-0 space-y-8">
-           {/* Hero Carousel */}
-            <div className="pt-3 pb-3">
-              <HeroCarousel />
-            </div>
-            
-            {/* Vehicle Finder */}
-            <div className="pt-3 pb-3">
-              <VehicleFinder />
-            </div>
+         <div className="max-w-[980px] mx-auto px-4 sm:px-6 lg:px-0 space-y-8">
+            {/* Hero Carousel */}
+             <div className="pt-4 pb-4">
+               <HeroCarousel />
+             </div>
+             
+            {/* Latest News */}
+           <div className="pt-4 pb-4">
+             <LatestNews />
+           </div>
            
-          {/* Latest News */}
-          <div className="pt-3 pb-3">
-            <LatestNews />
-          </div>
-          
-          {/* Latest Reviews */}
-          <div className="pt-3 pb-3">
-            <LatestReviews />
-          </div>
-          
-          {/* Buyer's Guide */}
-          <div className="pt-3 pb-3">
-            <BuyersGuide />
-          </div>
-          
-          {/* Featured Videos */}
-          <div className="pt-3 pb-3">
-            <FeaturedVideos />
-          </div>
-          
-          {/* Browse by Category */}
-          <div className="pt-3 pb-3">
-            <BrowseByCategory />
-          </div>
+           {/* Latest Reviews */}
+           <div className="pt-4 pb-4">
+             <LatestReviews />
+           </div>
+           
+           {/* Buyer's Guide */}
+           <div className="pt-4 pb-4">
+             <BuyersGuide />
+           </div>
+           
+            {/* Vehicle Finder */}
+             <div className="pt-4 pb-4">
+               <VehicleFinder />
+             </div>
+           
+           {/* Featured Videos */}
+           <div className="pt-4 pb-4">
+             <FeaturedVideos />
+           </div>
+           
+           {/* Browse by Category */}
+           <div className="pt-4 pb-4">
+             <BrowseByCategory />
+           </div>
         </div>
       </main>
     </div>;
