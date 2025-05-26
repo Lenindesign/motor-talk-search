d66f2b512d73fcae1cf93d988e0f7ddf5294e6fd
Lenin Aviles <lenin.aviles@hearst.com>
2025-05-25 19:14:59 -0700

    fix: reduce vertical spacing between components
    - Changed all component spacing from pt-6 pb-6 to pt-3 pb-3
    - Maintains consistent spacing throughout the page
    - Creates more compact layout

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -7,43 +7,44 @@ import BuyersGuide from '../components/home/BuyersGuide';
 import LatestReviews from '../components/home/LatestReviews';
 import FeaturedVideos from '../components/home/FeaturedVideos';
 import BrowseByCategory from '../components/home/BrowseByCategory';
+
 const Index = () => {
   return <div className="flex min-h-screen flex-col bg-motortrend-gray w-full">
       <GlobalHeader />
       <main className="flex flex-1 flex-col">
         <div className="max-w-[980px] mx-auto px-4 sm:px-6 lg:px-0 space-y-8">
            {/* Hero Carousel */}
-           <div className="pt-4 pb-1.5 py-[15px]">
-             <HeroCarousel />
-           </div>
+            <div className="pt-3 pb-3">
+              <HeroCarousel />
+            </div>
+            
+            {/* Vehicle Finder */}
+            <div className="pt-3 pb-3">
+              <VehicleFinder />
+            </div>
            
-           {/* Vehicle Finder */}
-           <div className="pt-1.5 pb-6 py-0">
-             <VehicleFinder />
-           </div>
-          
           {/* Latest News */}
-          <div className="pt-6 pb-6">
+          <div className="pt-3 pb-3">
             <LatestNews />
           </div>
           
           {/* Latest Reviews */}
-          <div className="pt-6 pb-6">
+          <div className="pt-3 pb-3">
             <LatestReviews />
           </div>
           
           {/* Buyer's Guide */}
-          <div className="pt-6 pb-6">
+          <div className="pt-3 pb-3">
             <BuyersGuide />
           </div>
           
           {/* Featured Videos */}
-          <div className="pt-6 pb-6">
+          <div className="pt-3 pb-3">
             <FeaturedVideos />
           </div>
           
           {/* Browse by Category */}
-          <div className="pt-6 pb-6">
+          <div className="pt-3 pb-3">
             <BrowseByCategory />
           </div>
         </div>
