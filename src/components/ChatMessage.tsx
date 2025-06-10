9392e2216795cb77a13a5253a95fb3e0cf002f37
Lenin Aviles <lenin.aviles@hearst.com>
2025-06-09 17:08:24 -0700

    Update avatars: replaced MT text with SVG logo and user icon with profile image

--- src/components/ChatMessage.tsx
+++ src/components/ChatMessage.tsx
@@ -34,7 +34,11 @@ const ChatMessage: React.FC<ChatMessageProps> = ({
       <div className={`flex max-w-[90%] sm:max-w-[80%]`}>
         {!isUser && (
           <div className="mr-2 flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-full bg-motortrend-red text-white">
-            MT
+            <img 
+              src="https://d2kde5ohu8qb21.cloudfront.net/files/68477633dcb7490008110dd5/mt-logo.svg" 
+              alt="MT Logo" 
+              className="w-4 h-4 brightness-0 invert"
+            />
           </div>
         )}
         <div
@@ -59,23 +63,12 @@ const ChatMessage: React.FC<ChatMessageProps> = ({
           )}
         </div>
         {isUser && (
-          <div className="ml-2 flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-full bg-gray-300 text-gray-600">
-            <svg
-              width="16"
-              height="16"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              className="sm:w-[18px] sm:h-[18px]"
-            >
-              <path
-                d="M20 21C20 18.2386 16.4183 16 12 16C7.58172 16 4 18.2386 4 21M12 13C9.23858 13 7 10.7614 7 8C7 5.23858 9.23858 3 12 3C14.7614 3 17 5.23858 17 8C17 10.7614 14.7614 13 12 13Z"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
+          <div className="ml-2 flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-full overflow-hidden">
+            <img 
+              src="https://d2kde5ohu8qb21.cloudfront.net/files/6839e7e53277480008013d30/greg.jpg" 
+              alt="User" 
+              className="w-full h-full object-cover"
+            />
           </div>
         )}
       </div>
