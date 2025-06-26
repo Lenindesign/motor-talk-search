bc4ab727f4ee1440c86c9a639a3cefb2c2a3234d
Lenin Aviles <lenin.aviles@hearst.com>
2025-06-26 13:41:56 -0700

    Add Tesla vehicles to database and remove SecondaryHeroCarousel

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,56 +1,47 @@
 import React from "react";
 import HeroCarousel from '../components/home/HeroCarousel';
-import SecondaryHeroCarousel from '../components/home/SecondaryHeroCarousel';
 import LatestNews from '../components/home/LatestNews';
 import VehicleFinder from '../components/home/VehicleFinder';
 import BuyersGuide from '../components/home/BuyersGuide';
 import LatestReviews from '../components/home/LatestReviews';
 import FeaturedVideos from '../components/home/FeaturedVideos';
 import BrowseByCategory from '../components/home/BrowseByCategory';
-
 const Index = () => {
-  return (
-    <div className="px-4 md:px-0">
-      {/* Hero Carousels */}
-      <div className="space-y-8 pt-0 sm:pt-8">
-        {/* Main Hero Carousel */}
-        <HeroCarousel />
-        
-        {/* Secondary Hero Carousel */}
-        <SecondaryHeroCarousel />
-      </div>
+  return <div className="px-4 md:px-0">
+            {/* Hero Carousel */}
+             <div className="mb-8 pt-0 sm:pt-8">
+               <HeroCarousel />
+             </div>
             
-      {/* Latest News */}
-      <div className="mb-8">
-        <LatestNews />
-      </div>
+            {/* Latest News */}
+            <div className="mb-8">
+              <LatestNews />
+            </div>
             
-      {/* Latest Reviews */}
-      <div className="mb-8">
-        <LatestReviews />
-      </div>
+            {/* Latest Reviews */}
+            <div className="mb-8">
+              <LatestReviews />
+            </div>
             
-      {/* Buyer's Guide */}
-      <div className="mb-8">
-        <BuyersGuide />
-      </div>
+            {/* Buyer's Guide */}
+            <div className="mb-8">
+              <BuyersGuide />
+            </div>
             
-      {/* Vehicle Finder */}
-      <div className="mb-8">
-        <VehicleFinder />
-      </div>
+            {/* Vehicle Finder */}
+            <div className="mb-8">
+              <VehicleFinder />
+            </div>
             
-      {/* Featured Videos */}
-      <div className="mb-8">
-        <FeaturedVideos />
-      </div>
+            {/* Featured Videos */}
+            <div className="mb-8">
+              <FeaturedVideos />
+            </div>
             
-      {/* Browse by Category */}
-      <div className="mb-8">
-        <BrowseByCategory />
-      </div>
-    </div>
-  );
+            {/* Browse by Category */}
+            <div className="mb-8">
+              <BrowseByCategory />
+            </div>
+    </div>;
 };
-
 export default Index;
